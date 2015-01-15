e182dc44bf12aefde79d67efb5016ceb29796c9e
sergeyt <stodyshev@gmail.com>
2015-01-16 02:28:38 +0600

    init

---
+++ request.go
@@ -0,0 +1,113 @@
+package httputil
+
+// Borrowed from drone.io
+
+import (
+	"net/http"
+	"strings"
+)
+
+// IsHTTPS is a helper function that evaluates the http.Request
+// and returns True if the Request uses HTTPS. It is able to detect,
+// using the X-Forwarded-Proto, if the original request was HTTPS and
+// routed through a reverse proxy with SSL termination.
+func IsHTTPS(r *http.Request) bool {
+	switch {
+	case r.URL.Scheme == "https":
+		return true
+	case r.TLS != nil:
+		return true
+	case strings.HasPrefix(r.Proto, "HTTPS"):
+		return true
+	case r.Header.Get("X-Forwarded-Proto") == "https":
+		return true
+	default:
+		return false
+	}
+}
+
+// GetScheme is a helper function that evaluates the http.Request
+// and returns the scheme, HTTP or HTTPS. It is able to detect,
+// using the X-Forwarded-Proto, if the original request was HTTPS
+// and routed through a reverse proxy with SSL termination.
+func GetScheme(r *http.Request) string {
+	switch {
+	case r.URL.Scheme == "https":
+		return "https"
+	case r.TLS != nil:
+		return "https"
+	case strings.HasPrefix(r.Proto, "HTTPS"):
+		return "https"
+	case r.Header.Get("X-Forwarded-Proto") == "https":
+		return "https"
+	default:
+		return "http"
+	}
+}
+
+var hostHeaders = []string{"X-Forwarded-For", "X-Host", "XFF"}
+
+// GetHost is a helper function that evaluates the http.Request
+// and returns the hostname. It is able to detect, using the
+// X-Forarded-For header, the original hostname when routed
+// through a reverse proxy.
+func GetHost(r *http.Request) string {
+	switch {
+	case len(r.Host) != 0:
+		return r.Host
+	case len(r.URL.Host) != 0:
+		return r.URL.Host
+	default:
+		for _, k := range hostHeaders {
+			var h = r.Header.Get(k)
+			if len(h) > 0 {
+				return h
+			}
+		}
+		return "localhost:8080"
+	}
+}
+
+// GetURL is a helper function that evaluates the http.Request
+// and returns the URL as a string. Only the scheme + hostname
+// are included; the path is excluded.
+func GetURL(r *http.Request) string {
+	return GetScheme(r) + "://" + GetHost(r)
+}
+
+// GetCookie retrieves and verifies the cookie value.
+func GetCookie(r *http.Request, name string) (value string) {
+	cookie, err := r.Cookie(name)
+	if err != nil {
+		return
+	}
+	value = cookie.Value
+	return
+}
+
+// SetCookie writes the cookie value.
+func SetCookie(w http.ResponseWriter, r *http.Request, name, value string) {
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     "/",
+		Domain:   r.URL.Host,
+		HttpOnly: true,
+		Secure:   IsHTTPS(r),
+	}
+
+	http.SetCookie(w, &cookie)
+}
+
+// DelCookie deletes a cookie.
+func DelCookie(w http.ResponseWriter, r *http.Request, name string) {
+	cookie := http.Cookie{
+		Name:   name,
+		Value:  "deleted",
+		Path:   "/",
+		Domain: r.URL.Host,
+		MaxAge: -1,
+	}
+
+	http.SetCookie(w, &cookie)
+}
