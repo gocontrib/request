69ba4658ef4dfe673f943d1ab5c04167b928c2c4
sergeyt <stodyshev@gmail.com>
2016-03-20 20:51:47 +0600

    fix for XFF headers

--- request.go
+++ request.go
@@ -7,23 +7,33 @@ import (
 	"strings"
 )
 
+// ParseHeader parses non unique headers value
+// from a http.Request and return a slice of the values
+// queried from the header
+func ParseHeader(r *http.Request, header string, token string) (val []string) {
+	for _, v := range r.Header[header] {
+		options := strings.Split(v, ";")
+		for _, o := range options {
+			keyvalue := strings.Split(o, "=")
+			var key, value string
+			if len(keyvalue) > 1 {
+				key, value = strings.TrimSpace(keyvalue[0]), strings.TrimSpace(keyvalue[1])
+			}
+			key = strings.ToLower(key)
+			if key == token {
+				val = append(val, value)
+			}
+		}
+	}
+	return
+}
+
 // IsHTTPS is a helper function that evaluates the http.Request
 // and returns True if the Request uses HTTPS. It is able to detect,
 // using the X-Forwarded-Proto, if the original request was HTTPS and
 // routed through a reverse proxy with SSL termination.
 func IsHTTPS(r *http.Request) bool {
-	switch {
-	case r.URL.Scheme == "https":
-		return true
-	case r.TLS != nil:
-		return true
-	case strings.HasPrefix(r.Proto, "HTTPS"):
-		return true
-	case r.Header.Get("X-Forwarded-Proto") == "https":
-		return true
-	default:
-		return false
-	}
+	return GetScheme(r) == "https"
 }
 
 // GetScheme is a helper function that evaluates the http.Request
@@ -31,40 +41,54 @@ func IsHTTPS(r *http.Request) bool {
 // using the X-Forwarded-Proto, if the original request was HTTPS
 // and routed through a reverse proxy with SSL termination.
 func GetScheme(r *http.Request) string {
+	v := ParseHeader(r, "Forwarded", "proto")
+	if len(v) != 0 && v[0] == "https" {
+		return "https"
+	}
 	switch {
+	case r.Header.Get("X-Forwarded-Proto") == "https":
+		return "https"
 	case r.URL.Scheme == "https":
 		return "https"
 	case r.TLS != nil:
 		return "https"
 	case strings.HasPrefix(r.Proto, "HTTPS"):
 		return "https"
-	case r.Header.Get("X-Forwarded-Proto") == "https":
-		return "https"
 	default:
 		return "http"
 	}
 }
-
-var hostHeaders = []string{"X-Forwarded-For", "X-Host", "XFF"}
 
 // GetHost is a helper function that evaluates the http.Request
 // and returns the hostname. It is able to detect, using the
 // X-Forarded-For header, the original hostname when routed
 // through a reverse proxy.
 func GetHost(r *http.Request) string {
+	v := ParseHeader(r, "Forwarded", "host")
+	if len(v) != 0 {
+		return v[0]
+	}
+	v = ParseHeader(r, "Forwarded", "for")
+	if len(v) != 0 {
+		// TODO add port?
+		return v[0]
+	}
 	switch {
+	case len(r.Header.Get("X-Forwarded-Host")) != 0:
+		return r.Header.Get("X-Forwarded-Host")
+	case len(r.Header.Get("X-Host")) != 0:
+		return r.Header.Get("X-Host")
+	case len(r.Header.Get("XFF")) != 0:
+		return r.Header.Get("XFF")
+	case len(r.Header.Get("X-Real-IP")) != 0:
+		return r.Header.Get("X-Real-IP")
 	case len(r.Host) != 0:
 		return r.Host
 	case len(r.URL.Host) != 0:
 		return r.URL.Host
 	default:
-		for _, k := range hostHeaders {
-			var h = r.Header.Get(k)
-			if len(h) > 0 {
-				return h
-			}
-		}
-		return "localhost:8080"
+		// TODO configurable default host
+		return "localhost:8000"
 	}
 }
 
